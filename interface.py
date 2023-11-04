a1b04849b0b4396dd818b56ebc06511def55ae36
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-11-05 00:05:24 +0800

    fix: worker interface is an abstract class

--- interface.py
+++ interface.py
@@ -1,12 +1,15 @@
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from worker_registry import worker_registry
 
 
-class WorkerInterface():
+class WorkerInterface(ABC):
     @abstractmethod
     def do(self, asset_data, *parameters):
         pass
 
     def __init_subclass__(cls, **kwargs) -> None:
+        """
+        automatically add Worker class to registry
+        """
         super().__init_subclass__(**kwargs)
         worker_registry.append(cls)

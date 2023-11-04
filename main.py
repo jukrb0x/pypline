4e6bbbb0d2eded136fe0896b605778df9c83267e
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2023-11-05 03:19:52 +0800

    fix: minor changes

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from manager import PipelineManager
 import pathlib
 
 asset_data = ["/Game/C"]
-atomics = {'asset_data': asset_data}
+ext_atomics = {'asset_data': asset_data}
+pipeline_config_file = pathlib.Path(__file__).parent / "pipeline.json"
 
-manager = PipelineManager(config_file=pathlib.Path(__file__).parent / "pipeline.json", atomics=atomics)
-manager.add_registered_jobs()
+if __name__ == "__main__":
+    manager = PipelineManager(config_file=pipeline_config_file, atomics=ext_atomics)
+    manager.add_registered_jobs()
 
-# todo: pass asset data and parameters from somewhere else
-parameters = (1, 2, "some_string", 1.234, ['element'], ('a', 'b'), {'k': 'v'})
+    # todo: pass asset data and parameters from somewhere else
+    parameters = (1, 2, "some_string", 1.234, ['element'], ('a', 'b'), {'k': 'v'})
 
-manager.execute_jobs(*parameters)
+    manager.execute_jobs(*parameters)
